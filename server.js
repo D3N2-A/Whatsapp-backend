760b890d98e8505d616e6bc657e905b2f9f90295
D3N2-A <anmolsharma999937@gmail.com>
2022-08-11 12:00:37 +0530

    pusher setup

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
 //app config
 const app = express();
 const port = process.env.PORT || 9000;
+
 //middleware
 app.use(express.json());
 //DB config
@@ -15,8 +17,37 @@ mongoose.connect(connection_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-//real time
 
+const pusher = new Pusher({
+  appId: "1461128",
+  key: "3a638ffc0a4c1fc6da12",
+  secret: "15dc18f2ee11a26b32f8",
+  cluster: "ap2",
+  useTLS: true,
+});
+
+//real time
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log(change);
+    if (change.operationType == "insert") {
+      const msgDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: msgDetails.user,
+        message: msgDetails.message,
+      });
+    } else {
+      console.log("Error Triggering pusher");
+    }
+  });
+});
 //api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
 
